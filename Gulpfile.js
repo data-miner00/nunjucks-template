b3f6a50b527217dc749b042778aa76b8d57120d1
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-07 08:53:40 +0800

    feat: Add global variable

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require("gulp");
 var nunjucks = require("gulp-nunjucks-render");
+var data = require("gulp-data");
 
 gulp.task("nunjucks", function () {
   return gulp
     .src("src/*")
+    .pipe(
+      data(function (file) {
+        return require("./settings.json");
+      })
+    )
     .pipe(nunjucks({ path: ["src/templates"], ext: ".html" }))
     .pipe(gulp.dest("dist"));
 });
