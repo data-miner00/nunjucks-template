83f4959046375ecdce96d9275113c1861dc5855c
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-10 21:05:20 +0800

    chore: Setup watch task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var { nunjucks, cssmin, uglify } = require("./tasks");
+var { CSS_PATH, JS_PATH, SRC_PATH } = require("./tasks/variables");
 
 gulp.task(
   "default",
@@ -9,3 +10,10 @@ gulp.task(
     uglify /* Use Higher order function when parameter needed */
   )
 );
+
+gulp.task("watch", function watchTask(done) {
+  gulp.watch(`${CSS_PATH}/*`, gulp.series(cssmin));
+  gulp.watch(`${JS_PATH}/*`, gulp.series(uglify));
+  gulp.watch(`${SRC_PATH}/*.njk`, gulp.series(nunjucks));
+  done();
+});
